8479c2f2dcbe7f2870609b4fa04889f8dd9ccde6
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-06-01 22:57:30 +0800

    chore: 6.1

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -28,11 +28,20 @@ func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
 	return r.todos, nil
 }
 
+func (r *todoResolver) User(ctx context.Context, obj *model.Todo) (*model.User, error) {
+	// panic(fmt.Errorf("not implemented"))
+	return &model.User{ID: obj.UserID, Name: "user " + obj.UserID}, nil
+}
+
 // Mutation returns generated.MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
 
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
+// Todo returns generated.TodoResolver implementation.
+func (r *Resolver) Todo() generated.TodoResolver { return &todoResolver{r} }
+
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
+type todoResolver struct{ *Resolver }
